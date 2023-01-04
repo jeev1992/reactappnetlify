98af483df294634ea69a87371795b07384296d7e
Jeevendra Singh <jeevendra.singh1992@gmail.com>
2023-01-04 21:19:00 +0530

    change from feature branch

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>This is Jeevendra's app!</h1>
       <p>This is a new feature</p>
+      <p>This is a code from feature branch</p>
     </div>
   );
 }
